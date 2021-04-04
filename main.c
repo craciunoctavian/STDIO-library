1f5e53dcf07a1f5a454705a082dfdf6b37389c0e
craciunoctavian <craciunoctavian99@yahoo.com>
2021-04-04 23:48:35 +0300

    so_fputc + so_fgetc

--- main.c
+++ main.c
@@ -2,4 +2,21 @@
 // Created by octi on 31.03.2021.
 //
 
+#include <stdio.h>
+#include "util/so_stdio.h"
 
+
+int main(){
+    SO_FILE *f = so_fopen("octi.txt", "r+");
+    printf("%c", so_fputc('c' ,f));
+    printf("%c", so_fputc('c' ,f));
+    printf("%c", so_fgetc(f));
+    printf("%c", so_fgetc(f));
+    printf("%c", so_fgetc(f));
+    printf("%c", so_fgetc(f));
+    printf("%c", so_fgetc(f));
+    printf("%c", so_fgetc(f));
+    printf("%d", so_fgetc(f));
+    so_fclose(f);
+    return 0;
+}
--- so_file.c
+++ so_file.c
@@ -3,17 +3,22 @@
 #include <unistd.h>
 #include "util/so_stdio.h"
 
+#define SIZE 4096
+
 struct _so_file {
     char *pathname;
     char *mode;
     char buffer[4096];
     int fd;
+    int cursor;
 };
 
 SO_FILE *so_fopen(const char *pathname, const char *mode) {
 
     SO_FILE *file = malloc(sizeof(SO_FILE));
     if (!file) return NULL;
+    memset(file->buffer, 0, 4096);
+    file->cursor = 0;
 
     file->pathname = malloc(strlen(pathname));
     if (!file->pathname) {
@@ -30,9 +35,8 @@ SO_FILE *so_fopen(const char *pathname, const char *mode) {
     }
     int fd = -1;
     strcpy(file->mode, mode);
-    // fd = open(file->pathname, O_WRONLY | O_CREAT, 0644);
     if (strcmp(mode, "r") == 0) {
-        fd = open(file->pathname, O_WRONLY);
+        fd = open(file->pathname, O_RDONLY);
     } else if (strcmp(mode, "r+") == 0) {
         fd = open(file->pathname, O_RDWR);
     } else if (strcmp(mode, "w") == 0) {
@@ -56,10 +60,31 @@ SO_FILE *so_fopen(const char *pathname, const char *mode) {
 }
 
 int so_fclose(SO_FILE *stream) {
+    if (stream == NULL) return SO_EOF;
     int r = close(stream->fd);
     if (r < 0) return SO_EOF;
     free(stream->pathname);
     free(stream->mode);
     free(stream);
     return 0;
+}
+
+int so_fgetc(SO_FILE *stream) {
+    int poz = stream->cursor % SIZE;
+    char c = stream->buffer[poz];
+    if (c == 0 || stream->cursor % SIZE == 0) {
+        int r = read(stream->fd, stream->buffer + poz, SIZE - poz);
+        if (r <= 0) return SO_EOF;
+        c = stream->buffer[poz];
+    }
+    stream->cursor++;
+    return (int) c;
+}
+
+int so_fputc(int c, SO_FILE *stream) {
+    stream->buffer[stream->cursor % SIZE] = (char) c;
+    int r = write(stream->fd, stream->buffer + stream->cursor % SIZE, 1);
+    if (r != 1) return SO_EOF;
+    stream->cursor++;
+    return c;
 }
